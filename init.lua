f783b0b647d74f8e23ccbbb590c6e8bfbb86a74c
sujit <ssutar@dexciss.com>
2024-09-27 12:19:03 +0530

    transparent background

--- init.lua
+++ init.lua
@@ -204,6 +204,9 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 	end,
 })
 vim.api.nvim_set_keymap("n", "<leader>a", ":ASToggle<CR>", {})
+-- Make Background Transparent
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
